7a40a8699215b2ce1e5bd7d1a6203eba1ce66eef
arieycliu <ariel.yc.liu@gmail.com>
2024-05-26 23:47:43 -0400

    Update refresh_calendars.js

--- refresh_calendars.js
+++ refresh_calendars.js
@@ -97,37 +97,3 @@ function refreshCalendar(calendar, calendarHeading, currentYear, currentMonth) {
         tbody.appendChild(row);
     }
 }
-
-// function refreshCalendarMini() {
-    
-//     const currentDateMini = new Date(currentYearMini, currentMonthMini, 1)
-//     const monthNameMini = currentDateMini.toLocaleString('default', { month: 'long' });
-//     const yearMini = currentDateMini.getFullYear();
-//     calendarHeadingMini.innerText = monthNameMini + ' ' + yearMini;
-
-//     const lastDayOfMonthMini = getLastDayOfMonth(currentYearMini, currentMonthMini);
-//     const firstDayOfMonthMini = new Date(currentYearMini, currentMonthMini, 1).getDay();
-
-//     let date = 1;
-//     const tbody = calendarMini.querySelector('tbody'); // table body
-//     tbody.innerHTML = ''; // clear previous content
-
-//     for (let i = 0; i < 6; i++) {
-//         const row = document.createElement('tr');
-//         for (let j = 0; j < 7; j++) {
-//             const cell = document.createElement('td');
-//             if (i === 0 && j < firstDayOfMonthMini) {  // empty cells before the start of the month
-//                 cell.classList.add('blank');
-//                 cell.textContent = '';
-//             } else if (date > lastDayOfMonthMini) {  // empty cells after the end of the month
-//                 cell.textContent = '';
-//                 break;
-//             } else {
-//                 cell.textContent = date;
-//                 date++;
-//             }
-//             row.appendChild(cell); // add cell to row
-//         }
-//         tbody.appendChild(row);
-//     }
-// }
