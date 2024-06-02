453689e56a774980c73dc59c49b1a4df93bab1ea
Ariel Liu <ariel.yc.liu@gmail.com>
2024-06-01 20:01:04 -0400

    KAN-9 Add associated table for tasks to tags
    Each task previously held a list of arrays.
    As I am not using PostgreSQL, arrays
    are not supported. Instead, an association
    table is created to support the many
    to many relationship that tags and tasks hold.

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,14 @@
 import datetime
 from config import db
 from typing import Optional, List
-from sqlalchemy import ARRAY, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+
+task_tags = db.Table (  # Association table for tasks to tags
+    'task_tags',
+    Column('task_id', Integer, ForeignKey('task.id')),
+    Column('tag_id', Integer, ForeignKey('tag.id'))
+)
 
 class Task(db.Model):
     __tablename__ = "task"
@@ -11,7 +17,6 @@ class Task(db.Model):
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[Optional[str]] = mapped_column(String(4096))
     type: Mapped[str] = mapped_column(String(20), nullable=False)  # indicate whether assignment or test type
-    tags: Mapped[List[str]] = mapped_column(ARRAY(String))  # subtypes enable finer task categories e.g, midterm, survey
     grade_weight: Mapped[int] = mapped_column(Integer)  # weight of task e.g, 5%
     grade_achieved: Mapped[int] = mapped_column(Integer)
     course_code: Mapped[str] = mapped_column(String(50))
@@ -19,8 +24,9 @@ class Task(db.Model):
     time_start: Mapped[datetime.datetime] = mapped_column(DateTime)
     time_end: Mapped[datetime.datetime] = mapped_column(DateTime)  # must end after start date
     subtask_ids: Mapped[List[int]] = mapped_column(ARRAY(Integer))  # one task can link to multiple subtasks
+    tags = db.relationship('Tag', secondary=task_tags, back_populates='tasks')  # subtypes enable finer task categories e.g, midterm, survey
 
-class SubTask(db.Model):
+class Subtask(db.Model):
     __tablename__ = "subtask"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -30,3 +36,10 @@ class SubTask(db.Model):
     status: Mapped[str] = mapped_column(String(20), nullable=False)  # status is independent of its parents
     time_start: Mapped[datetime.datetime] = mapped_column(DateTime)
     time_end: Mapped[datetime.datetime] = mapped_column(DateTime)
+
+class Tag(db.Model):
+    __tablename__ = "tag"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    tag_value: Mapped[str] = mapped_column(String(50), nullable=False)
+    tasks = db.relationship('Task', secondary=task_tags, back_populated='tags')
