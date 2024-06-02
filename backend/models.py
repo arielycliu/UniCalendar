73f4b5a78ca6103071c4b8349212257d6372a1e6
Ariel Liu <ariel.yc.liu@gmail.com>
2024-06-01 20:34:26 -0400

    FIX Refactor model to allow nullable fields
    Set all fields to nullable in the Task model to accommodate
    optional fields like description. Additionally, convert
    grade_weight and grade_achieve to Float type for more
    flexibility in storing numeric values.

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 import datetime
 from config import db
 from typing import Optional, List
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 task_tags = db.Table (  # Association table for tasks to tags
@@ -15,14 +15,14 @@ class Task(db.Model):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[Optional[str]] = mapped_column(String(4096))
+    description: Mapped[str] = mapped_column(String(4096), nullable=True)
     type: Mapped[str] = mapped_column(String(20), nullable=False)  # indicate whether assignment or test type
-    grade_weight: Mapped[int] = mapped_column(Integer)  # weight of task e.g, 5%
-    grade_achieved: Mapped[int] = mapped_column(Integer)
-    course_code: Mapped[str] = mapped_column(String(50))
-    status: Mapped[str] = mapped_column(String(20), nullable=False)  # status of task e.g, todo, doing, done, blocked
-    time_start: Mapped[datetime.datetime] = mapped_column(DateTime)
-    time_end: Mapped[datetime.datetime] = mapped_column(DateTime)  # must end after start date
+    grade_weight = mapped_column(Float, nullable=True)  # weight of task e.g, 5%
+    grade_achieved: Mapped[float] = mapped_column(Float, nullable=True)
+    course_code: Mapped[str] = mapped_column(String(50), nullable=True)
+    status: Mapped[str] = mapped_column(String(20), default='TODO')  # status of task e.g, todo, doing, done, blocked
+    time_start: Mapped[str] = mapped_column(String(50), default=str(datetime.datetime.now()))
+    time_end: Mapped[str] = mapped_column(String(50), nullable=True)  # must end after start date
     tags = db.relationship('Tag', secondary=task_tags, back_populates='tasks')  # subtypes enable finer task categories e.g, midterm, survey
 
 class Subtask(db.Model):
@@ -30,16 +30,16 @@ class Subtask(db.Model):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-    description: Mapped[Optional[str]] = mapped_column(String(4096))
+    description: Mapped[Optional[str]] = mapped_column(String(4096), nullable=True)
     # type must be identical to type of its task parent
-    status: Mapped[str] = mapped_column(String(20), nullable=False)  # status is independent of its parents
-    time_start: Mapped[datetime.datetime] = mapped_column(DateTime)
-    time_end: Mapped[datetime.datetime] = mapped_column(DateTime)
-    parent_task_id: Mapped[int]  # one task can link to multiple subtasks
+    status: Mapped[str] = mapped_column(String(20), default='TODO')  # status is independent of its parents
+    time_start: Mapped[str] = mapped_column(String(50), default=str(datetime.datetime.now()))
+    time_end: Mapped[str] = mapped_column(String(50), nullable=True)
+    parent_task_id: Mapped[int] = mapped_column(Integer, nullable=True)  # one task can link to multiple subtasks
 
 class Tag(db.Model):
     __tablename__ = "tag"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     tag_value: Mapped[str] = mapped_column(String(50), nullable=False)
-    tasks = db.relationship('Task', secondary=task_tags, back_populated='tags')
+    tasks = db.relationship('Task', secondary=task_tags, back_populates='tags')
