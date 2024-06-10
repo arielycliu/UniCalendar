c94a6a0526ae49c590bce1888601c434c148ff5a
arieycliu <ariel.yc.liu@gmail.com>
2024-06-10 16:02:15 -0400

    KAN-18 Add delete task functionality
    - removed alert with response message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -95,7 +95,7 @@ function App() {
 	return (
 		<>
 			<Calendar tasks={tasks} handleOpenCreateModal={handleOpenCreateModal} handleOpenUpdateModal={handleOpenUpdateModal}/>
-			<Floaters tasks={tasks} handleOpenUpdateModal={handleOpenUpdateModal} />
+			<Floaters tasks={tasks} setTasks={setTasks} handleOpenUpdateModal={handleOpenUpdateModal} />
 			<Colors colors={colors}/>
 			<CreateModal show={isCreateModalOpen} onCreateModalClose={onCreateModalClose} selectedDay={selectedDay} />
 			<UpdateModal show={isUpdateModalOpen} updateModalTask={updateModalTask} onUpdateModalClose={onUpdateModalClose} />
